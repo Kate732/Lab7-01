b46a61a4fd86e7c925ff7cfbe8f6758e0f9737d9
Kate732 <kmaistrenko@kse.org.ua>
2022-05-16 18:22:30 +0100

    split and check for correct spelling

---
+++ Lab7/Check.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Lab7
+{
+    public class Check
+    {
+        public static List<string> CheckOnTypos(string str)
+        {
+            string glossary = File.ReadAllText("words_list.txt");
+            string[] words = ConvertToArrayOfWords(str);
+            List<string> typos = new List<string>();
+            foreach (var word in words)
+            {
+                if (glossary.Contains(word) == false)
+                {
+                    typos.Add(word);
+                }
+            }
+
+            return typos;
+        }
+
+        private static string[] ConvertToArrayOfWords(String str)
+        {
+            var arrayStr = str.ToCharArray();
+            for (int i = 0; i < arrayStr.Length; i++)
+            {
+                if (i > 0 && i < arrayStr.Length - 1 && (arrayStr[i] == '-' || arrayStr[i] == '\'') &&
+                    (char.IsLetter(arrayStr[i - 1]) && char.IsLetter(arrayStr[i + 1])))
+                {
+                    continue;
+                }
+
+                if (char.IsPunctuation(arrayStr[i]))
+                {
+                    arrayStr[i] = ' ';
+                }
+            }
+
+            str = new string(arrayStr);
+            string[] splited = str.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            return splited;
+        }
+    }
+}
---
+++ Lab7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using static Lab7.Check;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> incorectWords = CheckOnTypos(Console.ReadLine());
+            foreach (var word in incorectWords)
+            {
+                Console.WriteLine(word);
+            }
+            Console.WriteLine(incorectWords.Count);
+        }
+    }
+}
